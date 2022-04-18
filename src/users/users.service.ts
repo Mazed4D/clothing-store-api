381b153ea353b5c420987cec39027ebb9433535c
Milan Paunovic <paunovic.milan@outlook.com>
2022-04-18 19:02:12 +0200

    removed creating of users from users controller

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -5,7 +5,6 @@ import {
 } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
-import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { User } from './entities/user.entity';
 
@@ -15,18 +14,6 @@ export class UsersService {
     @InjectRepository(User)
     private readonly repository: Repository<User>,
   ) {}
-
-  async create(createUserDto: CreateUserDto) {
-    const newUser = new User();
-    const existingUser = await this.repository.findOne({
-      where: { username: createUserDto.username },
-    });
-    if (existingUser) {
-      throw new BadRequestException('User with this username already exists.');
-    }
-    Object.assign(newUser, createUserDto);
-    return await this.repository.save(newUser);
-  }
 
   findAll() {
     return `This action returns all users`;
